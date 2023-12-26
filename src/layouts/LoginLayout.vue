896b3a0ccac2d01a4906af255e3c56f2b5ab8ec7
vas1levnv <vas1levnv@yandex.ru>
2023-12-26 17:28:24 +0300

    Initial commit

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <div class="form-wrapper">
+        <router-view/>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import {defineComponent, ref} from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.form {
+  &-wrapper {
+    max-width: 400px;
+    width: 100%;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-form
+    @submit="onSubmit"
+    class="flex column gap"
+    style="gap: 0.5rem"
+  >
+    <h5 class="text-center">Sign in</h5>
+
+    <q-input
+      filled
+      v-model="email"
+      label="Your email *"
+      lazy-rules
+      :rules="[ val => val && val.length > 0 || 'Please type correct email']"
+    />
+
+    <q-input
+      filled
+      type="password"
+      v-model="password"
+      label="Your password *"
+      lazy-rules
+      :rules="[
+          val => val && val.length > 6 || 'password must contain more than 6 characters'
+        ]"
+    />
+    <q-checkbox v-model="rememberMe" label="Remember me"/>
+    <q-btn label="Log in" @click="sendAuthForm" type="submit" color="primary"/>
+    <div class="flex justify-between">
+      <span>forgot your password</span>
+      <span>create an account</span>
+    </div>
+  </q-form>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const email = ref(null)
+const password = ref(null)
+const rememberMe = ref(null)
+
+const sendAuthForm = (e) => {
+  e.preventDefault()
+  console.log(email.value, password.value, rememberMe.value)
+}
+
+</script>
+
